810ecee3c01827e4d6c0100c435c95156aa2312e
m4nute <mgudinocontacto@gmail.com>
2023-05-01 20:09:14 -0300

    ✨ Auth and Account Components Finished

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
+  reactStrictMode: false,
 
   /**
    * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
