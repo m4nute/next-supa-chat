05f0fef42ff76a5fcfae0e27e5b6f971d723df8b
m4nute <mgudinocontacto@gmail.com>
2023-05-02 16:03:10 -0300

    ✨ Auth Component Name Change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,23 @@
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { type NextPage } from "next";
+import { useQuery } from "@tanstack/react-query";
+import { useUser } from "@supabase/auth-helpers-react";
 
 const Home: NextPage = () => {
   const supabase = useSupabaseClient();
+  const user = useUser();
+
+  async function getUserChats() {
+    return await supabase
+      .from("chats_users")
+      .select("chat_id")
+      .eq("user_id", user?.id);
+  }
+
+  const { isLoading, data, isError } = useQuery({
+    queryKey: ["getChats"],
+    queryFn: getUserChats,
+  });
 
   return (
     <div>
