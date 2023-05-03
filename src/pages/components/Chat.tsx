db85c03193c34f6be9d945d5ae6cbb0108069dcb
m4nute <mgudinocontacto@gmail.com>
2023-05-03 14:20:47 -0300

    ✨ Basic form for chat messages (RHF-Zod)

--- src/pages/components/Chat.tsx
+++ src/pages/components/Chat.tsx
@@ -1,3 +1,45 @@
+import { ZodType, z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 export default function Chat({ id }: { id: number }) {
-  return <div>{id}</div>;
+  type formData = {
+    message: string;
+  };
+
+  const schema: ZodType<formData> = z.object({
+    message: z.string().min(1).max(500),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<formData>({
+    resolver: zodResolver(schema),
+  });
+
+  const submitData = (data: formData) => {
+    reset();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(submitData)}>
+        <input
+          type="text"
+          placeholder="Send Message"
+          {...register("message")}
+          className="border bg-[#1c1c1c]"
+        />
+        <button type="submit">send</button>
+
+        <br />
+        {errors.message && (
+          <span className="text-red-500">{errors.message.message}</span>
+        )}
+      </form>
+    </div>
+  );
 }
