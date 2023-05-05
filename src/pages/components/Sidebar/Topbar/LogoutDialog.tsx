a9af6cf413dc53ba100102a30f39d01269ec37ae
m4nute <mgudinocontacto@gmail.com>
2023-05-05 20:32:25 -0300

    💄 Fixed Logout Dialog UI

--- src/pages/components/Sidebar/Topbar/LogoutDialog.tsx
+++ src/pages/components/Sidebar/Topbar/LogoutDialog.tsx
@@ -15,12 +15,14 @@ export default function LogoutDialog() {
       </AlertDialog.Trigger>
       <AlertDialog.Portal>
         <AlertDialog.Overlay className="fixed inset-0" />
-        <AlertDialog.Content className="fadeIn fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-[#2c2c2c] p-[25px]">
-          <AlertDialog.Title className="m-0 text-[17px] text-white">Are you absolutely sure?</AlertDialog.Title>
-          <AlertDialog.Description className="mb-5 mt-4 text-[15px] text-white">You will Logout.</AlertDialog.Description>
+        <AlertDialog.Content className="fadeIn fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-[#2c2c2c] p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+          <AlertDialog.Title className="m-0 text-[17px] font-medium text-white">Are you absolutely sure?</AlertDialog.Title>
+          <AlertDialog.Description className="mb-5 mt-4 text-[15px] leading-normal text-white">You will Logout.</AlertDialog.Description>
           <div className="flex justify-end gap-[25px]">
             <AlertDialog.Cancel asChild>
-              <button className="px-[15px]text-white inline-flex h-[35px] items-center justify-center rounded-[4px] bg-dialogButton transition-all hover:bg-dialogInput">Cancel</button>
+              <button className="hover:bg-mauve5 focus:shadow-mauve7 inline-flex h-[35px]  items-center justify-center rounded-[4px] bg-dialogButton px-[15px] font-medium leading-none text-white outline-none transition-all hover:bg-dialogInput focus:shadow-[0_0_0_2px]">
+                Cancel
+              </button>
             </AlertDialog.Cancel>
             <AlertDialog.Action asChild>
               <button
