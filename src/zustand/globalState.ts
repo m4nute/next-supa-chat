65dbb1e77d67d79be7367e37ccc007c31fe40dd8
m4nute <mgudinocontacto@gmail.com>
2023-05-06 17:19:28 -0300

    ✨ Updated GlobalState types

--- src/zustand/globalState.ts
+++ src/zustand/globalState.ts
@@ -1,20 +1,26 @@
-import { create } from "zustand";
+import { create } from "zustand"
 
 type Store = {
-  selectedChat: number | null;
-  selectedUser: any;
-};
+  selectedChat: number | null
+  selectedUser: { avatar_url: string | null; email: string } | null
+}
 
 type Setters = {
-  setSelectedChat: (chatId: number) => void;
-  setSelectedUser: (user: any) => void;
-};
+  setSelectedChat: (chatId: number) => void
+  setSelectedUser: (user: { email: string; avatar_url: string | null }) => void
+  reset: () => void
+}
 
-const useStore = create<Store & Setters>((set) => ({
+const initialState: Store = {
   selectedChat: null,
   selectedUser: null,
-  setSelectedChat: (chatId: number) => set(() => ({ selectedChat: chatId })),
-  setSelectedUser: (user: any) => set(() => ({ selectedUser: user })),
-}));
+}
 
-export default useStore;
+const useStore = create<Store & Setters>()((set) => ({
+  ...initialState,
+  setSelectedChat: (chatId: number) => set(() => ({ selectedChat: chatId })),
+  setSelectedUser: (user: { avatar_url: string | null; email: string }) => set(() => ({ selectedUser: user })),
+  reset: () => set(() => initialState),
+}))
+
+export default useStore
