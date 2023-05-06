5dfd6996f24e043909ee8373bb8da2b9ecc49e8f
m4nute <mgudinocontacto@gmail.com>
2023-05-06 17:20:24 -0300

    🎨 Made component for successful auth

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,32 +1,17 @@
 import { Auth } from "@supabase/auth-ui-react"
-// @ts-ignore
 import { ThemeSupa } from "@supabase/auth-ui-shared"
 import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react"
-import Link from "next/link"
+import SuccessAuth from "./components/Auth/SuccessAuth"
 
-const Home = () => {
+export default function Home() {
   const supabase = useSupabaseClient()
   const user = useUser()
 
-  if (user) {
-    return (
-      <div className="pt-10 text-center">
-        <h1 className="mb-4">Logged In Successfully!</h1>
-        <Link href="/" className="rounded-xl bg-message px-4 py-3 transition-all hover:bg-dialogInput">
-          Start Chatting
-        </Link>
-        <button className="ml-6 rounded-xl bg-message px-4 py-3 transition-all hover:bg-dialogInput" onClick={() => supabase.auth.signOut()}>
-          Logout
-        </button>
-      </div>
-    )
-  }
-
-  return (
+  return !user ? (
     <div className="xl-1/3 mx-auto w-5/6 pt-12 sm:w-4/5 md:w-3/5 lg:w-1/2">
       <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme="dark" providers={["google"]} redirectTo="http://localhost:3000/auth" />
     </div>
+  ) : (
+    <SuccessAuth />
   )
 }
-
-export default Home
