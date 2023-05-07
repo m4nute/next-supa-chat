fb382be22ee736ed2e4b579a05f032efeb479c52
m4nute <mgudinocontacto@gmail.com>
2023-05-07 11:18:01 -0300

    ✨ LastViewed update for every user_chat

--- src/pages/components/Sidebar/OpenedChats/SpecificChat/ChatCard.tsx
+++ src/pages/components/Sidebar/OpenedChats/SpecificChat/ChatCard.tsx
@@ -1,8 +1,12 @@
 import UserAvatar from "~/pages/components/UserAvatar"
 import useStore from "~/zustand/globalState"
+import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react"
+import { updateLastViewed } from "~/queries/allQueries"
 
 export default function ChatCard({ index, chat }: { index: number; chat: any }) {
   const [selectedChat, setSelectedChat, setSelectedUser] = useStore((state) => [state.selectedChat, state.setSelectedChat, state.setSelectedUser])
+  const user = useUser()
+  const supabase = useSupabaseClient()
 
   return (
     <li
@@ -11,11 +15,10 @@ export default function ChatCard({ index, chat }: { index: number; chat: any }) 
       onClick={() => {
         setSelectedChat(chat.chatId)
         setSelectedUser(chat.user)
+        updateLastViewed(supabase, chat.chatId, user?.id)
       }}>
       <UserAvatar avatarUrl={chat.user.avatar_url} email={chat.user.email} />
-      <h1 className="flex flex-col justify-center ml-2">
-        {chat.user.email.split("@")[0]} {chat.chatId}
-      </h1>
+      <h1 className="flex flex-col justify-center ml-2">{chat.user.email.split("@")[0]}</h1>
     </li>
   )
 }
