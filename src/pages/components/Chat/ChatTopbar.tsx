6531772adfcf3e130da6db8b87acac7ba38d8b51
m4nute <mgudinocontacto@gmail.com>
2023-05-07 15:03:39 -0300

    📱 GoBack button for <640px displays

--- src/pages/components/Chat/ChatTopbar.tsx
+++ src/pages/components/Chat/ChatTopbar.tsx
@@ -1,13 +1,20 @@
 import useStore from "~/zustand/globalState"
 import UserAvatar from "../UserAvatar"
+import { useWindowWidth } from "@react-hook/window-size"
+import GoBack from "./GoBack"
 
 export default function ChatTopbar() {
   const [selectedUser] = useStore((state) => [state.selectedUser])
 
+  const onlyWidth = useWindowWidth({ wait: 10 })
+
   return (
-    <div className="flex h-16 w-full  shadow-sm shadow-input bg-input px-4">
-      <UserAvatar avatarUrl={selectedUser?.avatar_url} email={selectedUser?.email} />
-      <h1 className="ml-2 flex flex-col justify-center">{selectedUser?.email}</h1>
+    <div className="flex h-16 w-full  shadow-sm shadow-input bg-input px-4 justify-between">
+      <div className="flex">
+        <UserAvatar avatarUrl={selectedUser?.avatar_url} email={selectedUser?.email} />
+        <h1 className="ml-2 flex flex-col justify-center">{selectedUser?.email}</h1>
+      </div>
+      {onlyWidth <= 640 && <GoBack />}
     </div>
   )
 }
